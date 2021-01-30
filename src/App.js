4d537d462e3606707222f746e5c17a4ad5228c88
Shikhar Baheti <shikharbaheti@gmail.com>
2021-01-30 13:35:27 -0600

    Router links

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container } from "react-bootstrap";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import NavBar from './components/Navbar/Navbar';
 import Landing from './components/Landing/Landing';
-import Login from './components/Landing/Login';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Landing />
-      <Login />
+      {/* <Landing /> */}
+      {/* <Login /> */}
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Landing />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,10 +4,10 @@ function NavBar() {
     return (
         <div className="Navbar">
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Kynect</Navbar.Brand>
+                <Navbar.Brand href="/">Kynect</Navbar.Brand>
                 <Nav className="mr-auto">
-                    <Nav.Link href="#features">Login</Nav.Link>
-                    <Nav.Link href="#pricing">Jobs</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                    <Nav.Link href="/jobs">Jobs</Nav.Link>
                 </Nav>
             </Navbar>
         </div>
