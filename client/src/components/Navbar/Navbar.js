477ede9087af0fb638a1ddb617ad3c15ff0a98cd
andres-blanco1785 <60301059+andres-blanco1785@users.noreply.github.com>
2021-01-31 07:53:57 -0600

    Navbar Changes (Needs Logo Still)

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,10 +1,15 @@
 import { Navbar, Nav } from "react-bootstrap";
+import navlogo from '../../images/1.png'
+import './Navbar.css';
+
 
 function NavBar() {
     return (
         <div className="Navbar">
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/">Kynect</Navbar.Brand>
+                <Navbar.Brand href="/">
+                  Kynect
+                </Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link href="/login">Login</Nav.Link>
                     <Nav.Link href="/jobs">Jobs</Nav.Link>
