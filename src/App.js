57f476c4cc72d78bf23a83c258ffabb40ac30ae4
andres-blanco1785 <60301059+andres-blanco1785@users.noreply.github.com>
2021-01-31 03:04:22 -0600

    Adding Builder to App.Js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Landing from './components/Landing/Landing';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import ErrorPage from './components/ErrorPage/ErrorPage';
+import Builder from './components/Builder/Builder';
 
 function App() {
   return (
@@ -25,6 +26,9 @@ function App() {
             </Route>
             <Route exact path="/register">
               <Register />
+            </Route>
+            <Route exact path="/builder">
+              <Builder />
             </Route>
             <Route exact path="/login">
               <Login />
